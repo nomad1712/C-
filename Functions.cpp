ee9a9e05c3d4dadf94fa8b20d14d99a5e626eb2c
nomad1712 <123312290+nomad1712@users.noreply.github.com>
2024-09-17 22:54:47 -0500

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+/*
+return_type function_name( any, parameters, you, have ) {
+
+   // Code block here
+
+   return output_if_there_is_any;
+
+}*/
+  void makesandwich() {
+
+  std::cout << "bread\n";
+  std::cout << "egg\n";
+  std::cout << "cheese\n";
+  std::cout << "avocado\n";
+  std::cout << "bread\n";
+
+}
+}
